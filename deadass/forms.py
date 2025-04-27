7044d585d69782a3120f661d778ef4e38de56d48
Tyler Allen <tyler@tallen.me>
2025-04-27 07:20:38 +0000

    Revert "add alphanumeric name form validation"
    This reverts commit d12630c054f79bc45032412895e130df30f41888.

--- deadass/forms.py
+++ deadass/forms.py
@@ -3,13 +3,8 @@
 # Author: Joe Abbate               #
 ####################################
 from flask_wtf import FlaskForm
-from wtforms import Form, SelectField, StringField, SubmitField, validators
-from wtforms.validators import DataRequired, StopValidation
-
-
-def is_alphanumeric(form, field):
-    if not field.data.isalnum():
-        raise StopValidation("Name must be alphanumeric")
+from wtforms import SubmitField, StringField, SelectField, Form
+from wtforms.validators import DataRequired
 
 
 class DBCreate(FlaskForm):
@@ -18,7 +13,7 @@ class DBCreate(FlaskForm):
         choices=[("POSTGRES", "PostgreSQL"), ("MYSQL", "MySQL"), ("MONGO", "Mongo")],
         validators=[DataRequired()],
     )
-    name = StringField("Database Name", validators=[DataRequired(), is_alphanumeric])
+    name = StringField("Database Name", validators=[DataRequired()])
     submit = SubmitField(("Submit"), validators=[DataRequired()])
 
     def validate(self, extra_validators=None):
@@ -26,9 +21,8 @@ class DBCreate(FlaskForm):
             return False
         return True
 
-
 class S3Create(FlaskForm):
-    name = StringField("S3 Username", validators=[DataRequired(), is_alphanumeric])
+    name = StringField("S3 Username", validators=[DataRequired()])
     submit = SubmitField(("Submit"), validators=[DataRequired()])
 
     def validate(self, extra_validators=None):
